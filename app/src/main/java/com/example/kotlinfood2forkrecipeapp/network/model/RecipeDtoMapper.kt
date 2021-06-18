74592b467c03979545f160f18a50736cadd1d37d
CyloTM <samuelchyke@hotmail.com>
2021-06-18 15:08:53 +0100

    Domain Mappers and Date Conversions

--- app/src/main/java/com/example/kotlinfood2forkrecipeapp/network/model/RecipeDtoMapper.kt
+++ app/src/main/java/com/example/kotlinfood2forkrecipeapp/network/model/RecipeDtoMapper.kt
@@ -2,6 +2,7 @@ package com.example.kotlinfood2forkrecipeapp.network.model
 
 import com.example.kotlinfood2forkrecipeapp.domain.model.Recipe
 import com.example.kotlinfood2forkrecipeapp.domain.util.DomainMapper
+import com.example.kotlinfood2forkrecipeapp.util.DateUtils
 
 
 class RecipeDtoMapper : DomainMapper<RecipeDto, Recipe> {
@@ -15,8 +16,8 @@ class RecipeDtoMapper : DomainMapper<RecipeDto, Recipe> {
             publisher = model.publisher,
             sourceUrl = model.sourceUrl,
             ingredients = model.ingredients,
-            dateAdded = model.dateAdded,
-            dateUpdated = model.dateUpdated,
+            dateAdded = DateUtils.longToDate(model.longDateAdded),
+            dateUpdated = DateUtils.longToDate(model.longDateUpdated),
         )
     }
 
@@ -29,8 +30,8 @@ class RecipeDtoMapper : DomainMapper<RecipeDto, Recipe> {
             publisher = domainModel.publisher,
             sourceUrl = domainModel.sourceUrl,
             ingredients = domainModel.ingredients,
-            dateAdded = domainModel.dateAdded,
-            dateUpdated = domainModel.dateUpdated,
+            longDateAdded = DateUtils.dateToLong(domainModel.dateAdded),
+            longDateUpdated = DateUtils.dateToLong(domainModel.dateUpdated),
         )
     }
 
