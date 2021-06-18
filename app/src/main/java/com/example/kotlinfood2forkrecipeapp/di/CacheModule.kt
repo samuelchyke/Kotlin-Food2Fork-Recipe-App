8cbea09039b399a83f8826e885f6c6c81fd1558c
CyloTM <samuelchyke@hotmail.com>
2021-06-18 14:49:32 +0100

    Providing the Room Database with Hilt

---
+++ app/src/main/java/com/example/kotlinfood2forkrecipeapp/di/CacheModule.kt
@@ -0,0 +1,32 @@
+package com.example.kotlinfood2forkrecipeapp.di
+
+import androidx.room.Room
+import com.example.kotlinfood2forkrecipeapp.cache.RecipeDao
+import com.example.kotlinfood2forkrecipeapp.cache.database.AppDatabase
+import com.example.kotlinfood2forkrecipeapp.presentation.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+    @Singleton
+    @Provides
+    fun provideDb(app: BaseApplication): AppDatabase {
+        return Room
+            .databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeDao(db: AppDatabase): RecipeDao {
+        return db.recipeDao()
+    }
+
+}
